ddb5e127c638f9d8b29a20382751657e718d982f
arthur_cg2 <conta.acg2002@gmail.com>
2021-02-22 11:21:30 -0300

    geração de tokens de autenticação

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,9 @@
 import connection from '../connection';
+import {
+  generateAcessToken,
+  generateRefreshToken,
+} from '../utils/authentication';
+
 import bcrypt from 'bcrypt';
 
 export default {
@@ -7,17 +12,23 @@ export default {
 
     if (!des_email || !des_senha) return res.sendStatus(400);
 
-    const doador = await connection('doador')
-      .select('id_doador')
-      .where('des_email', '=', des_email);
-    const ong = await connection('ong_contato')
-      .select('id_ong')
-      .where('des_email', '=', des_email);
+    const doador = (
+      await connection('doador')
+        .select('id_doador', 'nom_doador', 'link_img_perfil')
+        .where('des_email', '=', des_email)
+    )[0];
 
-    if (doador.length == 0 && ong.length == 0) return res.sendStatus(401);
+    const ong = (
+      await connection('ong_contato')
+        .select('ong_contato.id_ong', 'ong.nom_ONG', 'ong.seq_foto_perfil')
+        .join('ong', 'ong_contato.id_ong', '=', 'ong.id_ong')
+        .where('des_email', '=', des_email)
+    )[0];
 
-    const user = (doador || ong)[0];
-    const idName = doador.length != 0 ? 'id_doador' : 'id_ong';
+    if (!doador && !ong) return res.sendStatus(401);
+
+    const user = doador || ong;
+    const idName = doador ? 'id_doador' : 'id_ong';
 
     const userPassword = await connection('usuario')
       .select('des_senha')
@@ -31,7 +42,29 @@ export default {
     if (!match) return res.sendStatus(403);
 
     // jwt
+    let id, name, image;
+    if (idName == 'id_doador') {
+      id = user.id_doador;
+      name = user.nom_doador;
+      image = user.link_img_perfil;
+    } else {
+      id = user.id_ong;
+      name = user.nom_ONG;
+      image = (
+        await connection('foto')
+          .select('des_link')
+          .where('id_ong', '=', id)
+          .andWhere('seq_foto', '=', user.seq_foto_perfil)
+      )[0].des_link;
+    }
 
-    return res.sendStatus(200);
+    const accessToken = generateAcessToken({ id, name, image });
+    const refreshToken = generateRefreshToken({ id, name, image });
+
+    return res.json({
+      accessToken,
+      expiresIn: '15m',
+      refreshToken,
+    });
   },
 };
