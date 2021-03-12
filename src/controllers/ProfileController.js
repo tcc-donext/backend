04090486c0e7ed22a8d23ee9ddbb8ae950f9d14d
arthur_cg2 <conta.acg2002@gmail.com>
2021-03-12 13:02:42 -0300

    try no profilecontroller

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -4,10 +4,14 @@ export default {
   async index(request, response) {
     const { id_ong } = request.params;
 
-    const camp_ong = await connection('campanha')
-      .where('id_ong', id_ong)
-      .select('*');
-
-    return response.json(camp_ong);
+    try {
+      const camp_ong = await connection('campanha')
+        .where('id_ong', id_ong)
+        .select('*');
+      return response.json(camp_ong);
+    } catch (err) {
+      console.log(err);
+      return response.sendStatus(500);
+    }
   },
 };
